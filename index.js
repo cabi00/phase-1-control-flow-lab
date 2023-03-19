c90854053c6cb66b387a59dc684af5b5c067ae8d
vanessa garcia <venessag0908@gmail.com>
2023-03-18 18:13:11 -0600

    controlflowlab

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+//let giveCustomerFreeSample, charges20Dollars, Charges30Dollars;
+
+if (ride <= 400) {
+  //giveCustomerFreeSample = true;
+  return 'This one is on me!';
+} 
+else if (ride < 2000) {
+  return 'That will be twenty bucks.';
+} 
+else if (ride < 2500) {
+  return 'I will gladly take your thirty bucks.';
+}
+else { 
+  return 'No can do.';
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC"){
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response = "";
+  switch(tip){
+    case 'generous':
+      response = "Thank you so much.";
+      break;
+    case 'not as generous':
+      response = "Thank you.";
+      break;
+    default :
+      response = "Bye."
+      break;
+
+
+  }
+  return response;
 }
